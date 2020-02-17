e54204cc63875509b176d69537b22309870da734
eubeco <36170053+eubeco@users.noreply.github.com>
2020-02-17 10:22:50 +0100

    Corazón animado en el botón FAB, cuando lo pulsas el corazón gira.

--- app/src/main/java/com/example/recyclerview_staggeredgrid_fab/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview_staggeredgrid_fab/MainActivity.kt
@@ -1,20 +1,27 @@
 package com.example.recyclerview_staggeredgrid_fab
 
+import android.graphics.drawable.AnimatedVectorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.recyclerview.widget.DefaultItemAnimator
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
 
         val items = ArrayList<Tarjeta>()
@@ -37,6 +44,19 @@ class MainActivity : AppCompatActivity() {
         adaptador.onClick = {
             Toast.makeText(this@MainActivity, ""+ recView.getChildAdapterPosition(it), Toast.LENGTH_LONG).show()
         }
+
+        val fab = findViewById(R.id.FAB1) as FloatingActionButton
+        fab.setImageResource(R.drawable.ic_heart)
+        fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val rotar = getDrawable(R.drawable.heart_rotation) as AnimatedVectorDrawable?
+                fab.setImageDrawable(rotar)
+                rotar!!.start()
+                Snackbar.make(view!!, "Probando...", Snackbar.LENGTH_LONG).show()
+            }
+        })
+        
+
 
         recView.itemAnimator = DefaultItemAnimator()
 
